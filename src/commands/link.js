bd4f6455afd5f23c18d2b79fd936d16abf0475c9
duque das edits <92229705+JosueHTML2@users.noreply.github.com>
2025-02-16 12:18:24 +0000

    Adding new LINK command

---
+++ src/commands/link.js
@@ -0,0 +1,39 @@
+const { EmbedBuilder, Message, PermissionFlagsBits, Colors, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
+const BotClient = require("..");
+const myColous = require("../structures/colours");
+
+module.exports = {
+    name: "link", // Command name
+    description: "Manda uma embed com o link do server",
+    usage: "`!link`",
+
+    /**
+     * @param {Message} message 
+     * @param {string[]} args 
+     * @param {BotClient} client 
+     */
+    execute(message, args, client) {
+        const embed = new EmbedBuilder()
+        .setDescription("# https://discord.gg/HApztNnbvw")
+        .setColor(myColous.bright_blue_ocean)
+        .setTimestamp()
+        .setFooter({ text: "Por APOSTAS" });
+
+        const linkButton = new ButtonBuilder()
+        .setEmoji("🔗")
+        .setStyle(ButtonStyle.Link)
+        .setURL("https://discord.gg/HApztNnbvw")
+        .setLabel("DISCORD");
+
+        const row = new ActionRowBuilder().addComponents(linkButton);
+
+        message.channel.send({ embeds: [embed], components: [row] });
+    },
+    sendTemporaryMessage(msg, content) {
+        msg.reply(content).then(mg => {
+            setTimeout(() => {
+                mg.delete();
+            }, 2000);
+        });
+    }
+};
