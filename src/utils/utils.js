5680ba92813808969fcfdb15da13e05e566de945
duque das edits <92229705+JosueHTML2@users.noreply.github.com>
2025-03-29 14:58:53 +0000

    Update utils.js

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -7,39 +7,39 @@ class Utils {
     static get addCredit() {
         return require("./functions/addPoints");
     }
-    
+
     static get addLoss() {
         return require("./functions/addLoss");
     }
-    
+
     static get addWin() {
         return require("./functions/addWin");
     }
-    
+
     static get createMatch() {
         return require("./functions/createMatch");
     }
-    
+
     static get createMatchChannel() {
         return require("./functions/createMatchChannel");
     }
-    
+
     static get removeCredit() {
         return require("./functions/removePoints");
     }
-    
+
     static get removeLoss() {
         return require("./functions/removeLoss");
     }
-    
+
     static get removeWin() {
         return require("./functions/removeWin");
     }
-    
+
     static get removeWinMatch() {
         return require("./functions/removeWinMatch");
     }
-    
+
     static get returnServerRank() {
         return require("./functions/returnServerRank");
     }
@@ -49,15 +49,15 @@ class Utils {
     static get sendMatchEmbed() {
         return require("./functions/sendMatchEmbed");
     }
-    
+
     static get sendReply() {
         return require("./functions/sendReply");
     }
-    
+
     static get setMatchWinner() {
         return require("./functions/setMatchWinner");
     }
-    
+
     static get updateMembers() {
         return require("./functions/updateMembers");
     }
@@ -66,7 +66,7 @@ class Utils {
         return {
             btnWinner_handler: require("./handlers/btnWinner_handler"),
             endMatch_handler: require("./handlers/endMatch_handler"),
-            entermatch_handler: require(path.join(__dirname, "_handlers/enterMatch_handler")),
+            entermatch_handler: require("./handlers/enterMatch_handler"),
             handleMatchSelectMenu: require("./handlers/handleMatchSelectMenu"),
             outmatch_handler: require("./handlers/outmatch_handler"),
         };
