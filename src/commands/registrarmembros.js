b500d39cc3f59dbe1171cc57744ff840683c4491
duque das edits <92229705+duque7x@users.noreply.github.com>
2025-04-13 19:17:30 +0100

    refactor: one role instead of 2

--- src/commands/registrarmembros.js
+++ src/commands/registrarmembros.js
@@ -13,18 +13,16 @@ module.exports = {
      * @param {BotClient} client
      */
     async execute(interaction, client) {
+        interaction.guild.members.fetch();
         await interaction.reply({ content: "# Registrando...", flags: 64 });
         const members = interaction.guild.members.cache;
-        const roleIds = ["1338983241759064228", "1350144276834680912"];
 
         for (const member of members.values()) {
-            if (member.user.bot) return; // Skip bots
-
-            for (let roleId of roleIds) {
-                if (!member.roles.cache.has(roleId)) {
-                    await member.roles.add(roleId).catch(console.error);
-                }
+            if (member.user.bot) return; 
+            if (!member.roles.cache.has("1338983241759064228")) {
+                await member.roles.add("1338983241759064228").catch(console.error);
             }
+
             await User.findOneAndUpdate(
                 { "player.id": member.user.id },
                 { $set: { player: { name: member.user.username, id: member.user.id } } }, // Ensures update
