0dc0972dcf842e9eff5842a68c6a748e84f42d45
duque das edits <92229705+duque7x@users.noreply.github.com>
2025-04-13 16:59:00 +0100

    feat: dailypoints

--- src/utils/functions/addPoints.js
+++ src/utils/functions/addPoints.js
@@ -3,5 +3,12 @@ const User = require("../../structures/database/User");
 module.exports = async (userId, amount) => {
     const userProfile = await User.findOrCreate(userId);
     userProfile.points = userProfile.points + amount;
+
+    const dailyIndex = userProfile.dailyPoints.findIndex(entry => entry.date === today);
+    if (dailyIndex >= 0) {
+        userProfile.dailyPoints[dailyIndex].points += amount;
+    } else {
+        userProfile.dailyPoints.push({ date: today, points: amount });
+    }
     return await userProfile.save();
 }
--- src/utils/functions/removePoints.js
+++ src/utils/functions/removePoints.js
@@ -4,6 +4,13 @@ module.exports = async (userId, amount) => {
     const userProfile = await User.findOrCreate(userId);
     userProfile.points = userProfile.points - amount;
     userProfile.points = Math.max(0, userProfile.points);
-    
+
+    const dailyIndex = userProfile.dailyPoints.findIndex(entry => entry.date === today);
+    if (dailyIndex >= 0) {
+        userProfile.dailyPoints[dailyIndex].points += amount;
+    } else {
+        userProfile.dailyPoints.push({ date: today, points: amount });
+    }
+
     return await userProfile.save();
 }
